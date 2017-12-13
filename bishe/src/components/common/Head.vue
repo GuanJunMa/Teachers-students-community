d54e01f158c22ff6dc389bce791a5fd200b5d8b5
GuanJunMa <858324356@qq.com>
2017-12-13 10:46:00 +0800

    修改头部logo

--- bishe/src/components/common/Head.vue
+++ bishe/src/components/common/Head.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-      <el-col style="height:65px;position:fixed;top:0px;left:0px;background:#fefefe;border:1px solid #606060;"></el-col>
+      <el-col style="height-line:65px;position:fixed;top:0px;left:0px;background:#fefefe;text-align:left;">
+        <el-col :span="2">&nbsp;</el-col>
+        <i class="iconfont icon-school3" style="font-size:38px;float:left;"></i>
+        <el-col :span="4" style="color:#606060;margin-left:10px;">
+          <el-row style="font-weight:bold;">Community</el-row>
+          <el-row style="font-size:14px;">For students & Teacher</el-row>
+        </el-col>
+      </el-col>
   </div>
 </template>
