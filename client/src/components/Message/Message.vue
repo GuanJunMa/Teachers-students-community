e53f17e1ba434d43b2c16863ef96df80ebc2de15
GuanJunMa <maguanjun@lanlingdai.net>
2018-03-23 02:01:05 +0800

    完成作业界面

--- client/src/components/Message/Message.vue
+++ client/src/components/Message/Message.vue
@@ -1,22 +1,56 @@
 <template>
   <div>
-    <div style="height:200px;">123</div>
-    <el-button type="primary">英语</el-button>
-    <el-button type="primary">高数</el-button>
-    <el-button type="primary">专业课</el-button>
-    <el-button type="primary">体育</el-button>
-    <el-button type="primary">劳动</el-button>
-    <el-button type="primary">音乐</el-button>
-    <el-col span="18" :offset="3" style="border:1px solid;">
-      <el-col span="16" style="border:1px solid;">
-        <el-col style="border:1px solid;height:150px;">12313</el-col>
-        <el-col style="border:1px solid;height:200px;">12313</el-col>
-        <el-col style="border:1px solid;height:200px;">12313</el-col>
-        <el-col style="border:1px solid;height:200px;">12313</el-col>
+    <div style="height:100px;">&nbsp;</div>
+    <el-col span="18" :offset="3" style="border:1px solid #E4E7ED;">
+      <el-col span="16">
+        <el-tabs v-model="this.activeName" type="card" @tab-click="handleClick">
+          
+          <el-tab-pane label="C语言" name="first" style="text-align:left;">
+            <el-col v-for="item in getC" :key="item.id" style="margin-left:5%;width:90%;">
+              <el-col style="border:1px solid;line-height:50px;font-weight:bold;padding-left:10px;">{{item.title}}</el-col>
+              <el-col style="border:1px solid;color:#606266;padding:10px;">{{item.content}}</el-col>
+              <el-col style="height:50px;">&nbsp;</el-col>
+            </el-col>
+          </el-tab-pane>
+
+          <el-tab-pane label="高等数学" name="second" style="text-align:left;">
+            <el-col v-for="item in getMath" :key="item.id" style="margin-left:5%;width:90%;">
+              <el-col style="border:1px solid;line-height:50px;font-weight:bold;padding-left:10px;">{{item.title}}</el-col>
+              <el-col style="border:1px solid;color:#606266;padding:10px;">{{item.content}}</el-col>
+              <el-col style="height:50px;">&nbsp;</el-col>
+            </el-col>
+          </el-tab-pane>
+
+          <el-tab-pane label="通信原理" name="third" style="text-align:left;">
+            <el-col v-for="item in getCommunicatopn" :key="item.id" style="margin-left:5%;width:90%;">
+              <el-col style="border:1px solid;line-height:50px;font-weight:bold;padding-left:10px;">{{item.title}}</el-col>
+              <el-col style="border:1px solid;color:#606266;padding:10px;">{{item.content}}</el-col>
+              <el-col style="height:50px;">&nbsp;</el-col>
+            </el-col>
+          </el-tab-pane>
+
+          <el-tab-pane label="大学英语" name="fourth" style="text-align:left;">
+            <el-col v-for="item in getEnglish" :key="item.id" style="margin-left:5%;width:90%;">
+              <el-col style="border:1px solid;line-height:50px;font-weight:bold;padding-left:10px;">{{item.title}}</el-col>
+              <el-col style="border:1px solid;color:#606266;padding:10px;">{{item.content}}</el-col>
+              <el-col style="height:50px;">&nbsp;</el-col>
+            </el-col>
+          </el-tab-pane>
+        </el-tabs>
       </el-col>
-      <el-col span="7" :offset="1" style="border:1px solid;">
-        <el-col style="border:1px solid;height:150px;"><img src="../../images/1.jpg" width="100%" height="100%" alt=""></el-col>
-        <el-col style="border:1px solid;height:2000px;"></el-col>
+      <el-col span="7" :offset="1" style="border:1px solid #DCDFE6;">
+        <el-col style="height:150px;"><img src="../../images/1.jpg" width="100%" height="100%" alt=""></el-col>
+        <el-col style="height:500px;">
+          <el-col style="height:150px;">
+            <el-col style="line-height:50px;font-weight:bold;text-align:left;padding-left:10px;color:#E6A23C;">重要通知！</el-col>
+            <el-col style="height:100px;text-align:left;padding-left:10px;font-size:14px;">下周三下午三点，南体育场进行体育测试，请大家准时检录。</el-col>
+          </el-col>
+          <el-col style="height:150px;">
+            <el-col style="line-height:50px;font-weight:bold;text-align:left;padding-left:10px;color:#E6A23C;">重要通知！</el-col>
+            <el-col style="height:100px;text-align:left;padding-left:10px;font-size:14px;">下周四上午三四两节课将进行高数期中测评。请大家做好复习准备。</el-col>
+          </el-col>
+          <el-col style="height:500px;"></el-col>
+        </el-col>
       </el-col>
     </el-col>
   </div>
@@ -25,22 +59,70 @@
 export default {
   data () {
     return {
+      activeName: 'first',
       formData: {
         name: '',
         password: ''
-      }
+      },
+      cList: [],
+      mathList: [],
+      englishList: [],
+      communicationsList: []
     }
   },
   mounted () {
-    console.log(this.$route.path)
+    this.getList('cList')
+    this.getList('englishList')
+    this.getList('communicationsList')
+    this.getList('mathList')
   },
   computed: {
     showLogin () {
       return this.$route.path === '/Login'
+    },
+    getC() {
+      return this.cList
+    },
+    getMath() {
+      return this.mathList
+    },
+    getEnglish() {
+      return this.englishList
+    },
+    getCommunicatopn() {
+      return this.communicationsList
     }
   },
   methods: {
-    Login () {
+    handleClick() {},
+    getList(subject) {
+      var self = this;
+      this.$http.get("http://displaydata.cn").then(
+        successData => {
+          switch (subject) {
+            case 'cList':
+              self.cList = successData.body.cList
+              break;
+            case 'mathList':
+              self.mathList = successData.body.mathList
+              break;
+            case 'englishList':
+              self.englishList = successData.body.englishList
+              break;
+            case 'communicationsList':
+              self.communicationsList = successData.body.communicationsList
+              break;
+          
+            default:
+              break;
+          }
+        },
+        fileData => {
+          console.log(fileData);
+        }
+      );
+    },
+    Login() {
       this.$router.push('/')
     }
   }
--- client/src/util/mock.js
+++ client/src/util/mock.js
@@ -2,7 +2,7 @@ import Mock from 'mockjs'
 
 export default Mock.mock('http://displaydata.cn', {
 
-// 定义首页消息
+  // 定义首页消息
   newsData: [
     {
       'id': 1,
@@ -74,7 +74,7 @@ export default Mock.mock('http://displaydata.cn', {
     }
   ],
 
-    // 动态数据
+  // 动态数据
   speakList: [
     {
       'id': 1,
@@ -88,5 +88,142 @@ export default Mock.mock('http://displaydata.cn', {
         'image2'
       ]
     }
+  ],
+
+  cList: [
+    {
+      'title': '实现回文数算法',
+      'content': '  “回文”是指正读反读都能读通的句子，它是古今中外都有的一种修辞方式和文字游戏，如“我为人人，人人为我”等。在数学中也有这样一类数字有这样的特征，成为回文数（palindrome number）。设n是一任意自然数。若将n的各位数字反向排列所得自然数n1与n相等，则称n为一回文数。例如，若n=1234321，则称n为一回文数；但若n=1234567，则n不是回文数。',
+      'id': 1,
+      'image': 'huiwen'
+    }, {
+      'title': '打印出所有的“水仙花数”',
+      'content': '所谓“水仙花数”是指一个三位数，其各位数字立方和等于该数本身。例如：153是一个“水仙花数”，因为153=1的三次方＋5的三次方＋3的三次方。',
+      'id': 2,
+      'image': 'daozhi'
+    }, {
+      'title': '求所有‘完数’',
+      'content': '一个数如果恰好等于它的因子之和，这个数就称为“完数”。例如6=1＋2＋3.编程找出1000以内的所有完数。',
+      'id': 3,
+      'image': 'jinzita'
+    }, {
+      'title': '找规律',
+      'content': '有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？',
+      'id': 4,
+      'image': 'jinzita'
+    }, {
+      'title': '物理问题',
+      'content': '一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在第10次落地时，共经过多少米？第10次反弹多高？',
+      'id': 5,
+      'image': 'jinzita'
+    }, {
+      'title': '猴子摘桃',
+      'content': '一只猴子摘了N个桃子第一天吃了一半又多吃了一个,第二天又吃了余下的一半又多吃了一个,到第十天的时候发现还有一个.',
+      'id': 6,
+      'image': 'jinzita'
+    }, {
+      'title': '鸡兔同笼',
+      'content': '有若干只鸡和兔在同个笼子里，从上面数，有S个头；从下面数，有N只脚。求笼中各有几只鸡和几只兔？',
+      'id': 7,
+      'image': 'jinzita'
+    }
+  ],
+
+  mathList: [
+    {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }, {
+      'title': '求极限练习题',
+      'content': '123',
+      'id': 1,
+      'image': 'jixian'
+    }
+  ],
+
+  englishList: [
+    {
+      'title': '阅读翻译:The dogs that protect little penguins ',
+      'content': "The dogs that protect little penguins  Middle Island, a beautiful, rugged and windswept outcrop off the coast of southern Victoria is home to a colony of the world's smallest penguins. Originally known as fairy penguins, before some pen-pusher deemed that politically incorrect, they've now been given the far more dreary sounding title of little penguins. To be fair, they are just that - little, standing at 30 to 40cm tall.  There used to be hundreds of them on Middle Island - but that was before the foxes got to them. We went from a point where we had around 800 penguins down to where we could only find four,says Peter Abbott from the Penguin Preservation Project. In our biggest bird kill, we found 360 birds killed over about two nights. Foxes are thrill killers. They'll kill anything they can find. That particular incident was in 2005, but the problem had been building up for a few years. Middle Island - which is uninhabited by humans - is separated from the mainland by a stretch of water measuring no more than 20 or 30m.      At low tide, and when sand builds up in the narrow channel, foxes can cross from the mainland barely getting their paws wet.  The problem first became apparent in the year 2000 when the sea's natural current led to increased sand build-up.  Over time the fox population grew as it became clear they had an easy source of food. The fairy penguins, as I'm going to call them, faced being wiped out on Middle Island - until a chicken farmer, by the made-for-cinema name of Swampy Marsh, came up with a plan. He suggested sending one of his Maremma dogs to protect the birds. In Australia those dogs are generally used for chicken protection or goats or sheep,says Abbott.  The dog, the first of several to be used on Middle Island, was called Oddball - and Oddball made quite an impact.  We immediately saw a change in the pattern of the foxes,says Abbott.  Leading up to when the dog went on the island, every morning we'd find fox prints on the beach. Putting a dog on the island changed the hierarchy. The foxes can hear the dogs barking, they can smell them so they go somewhere else. Amazingly, since Oddball and his four-legged successors were introduced 10 years ago, there has not been a single penguin killed by a fox on Middle Island.  The fairy penguin population has gone back up to almost 200.  The current dogs patrolling Middle Island are Eudy and Tula, named after the scientific term for the fairy penguin: Eudyptula.  They are the sixth and seventh dogs to be used and a new puppy is being trained up by Peter Abbott and his team to start work in 2016.  The dogs operate in the penguin's breeding season, usually from October to March, when they spend five or six days a week on the island.  Even when the dogs are not there, their lingering scent is enough to keep the foxes away.",
+      'id': 1,
+      'image': 'fanyi'
+    }, {
+      'title': '阅读翻译:The dogs that protect little penguins ',
+      'content': "The dogs that protect little penguins  Middle Island, a beautiful, rugged and windswept outcrop off the coast of southern Victoria is home to a colony of the world's smallest penguins. Originally known as fairy penguins, before some pen-pusher deemed that politically incorrect, they've now been given the far more dreary sounding title of little penguins. To be fair, they are just that - little, standing at 30 to 40cm tall.  There used to be hundreds of them on Middle Island - but that was before the foxes got to them. We went from a point where we had around 800 penguins down to where we could only find four,says Peter Abbott from the Penguin Preservation Project. In our biggest bird kill, we found 360 birds killed over about two nights. Foxes are thrill killers. They'll kill anything they can find. That particular incident was in 2005, but the problem had been building up for a few years. Middle Island - which is uninhabited by humans - is separated from the mainland by a stretch of water measuring no more than 20 or 30m.      At low tide, and when sand builds up in the narrow channel, foxes can cross from the mainland barely getting their paws wet.  The problem first became apparent in the year 2000 when the sea's natural current led to increased sand build-up.  Over time the fox population grew as it became clear they had an easy source of food. The fairy penguins, as I'm going to call them, faced being wiped out on Middle Island - until a chicken farmer, by the made-for-cinema name of Swampy Marsh, came up with a plan. He suggested sending one of his Maremma dogs to protect the birds. In Australia those dogs are generally used for chicken protection or goats or sheep,says Abbott.  The dog, the first of several to be used on Middle Island, was called Oddball - and Oddball made quite an impact.  We immediately saw a change in the pattern of the foxes,says Abbott.  Leading up to when the dog went on the island, every morning we'd find fox prints on the beach. Putting a dog on the island changed the hierarchy. The foxes can hear the dogs barking, they can smell them so they go somewhere else. Amazingly, since Oddball and his four-legged successors were introduced 10 years ago, there has not been a single penguin killed by a fox on Middle Island.  The fairy penguin population has gone back up to almost 200.  The current dogs patrolling Middle Island are Eudy and Tula, named after the scientific term for the fairy penguin: Eudyptula.  They are the sixth and seventh dogs to be used and a new puppy is being trained up by Peter Abbott and his team to start work in 2016.  The dogs operate in the penguin's breeding season, usually from October to March, when they spend five or six days a week on the island.  Even when the dogs are not there, their lingering scent is enough to keep the foxes away.",
+      'id': 2,
+      'image': 'fanyi'
+    }
+  ],
+
+  communicationsList: [
+    {
+      'title': '简答题',
+      'content': '简述脉冲编码调制的主要过程？',
+      'id': 1,
+      'image': 'fanyi'
+    }, {
+      'title': '简答题',
+      'content': '随参信道传输媒质的特点？',
+      'id': 1,
+      'image': 'fanyi'
+    }, {
+      'title': '解答题',
+      'content': '设信息序列为100000000001100001，试编为AMI 码和HDB3码（第一个非零码编为+1），并画出相应波形。',
+      'id': 1,
+      'image': 'fanyi'
+    }, {
+      'title': '解答题',
+      'content': '已知调制信号载波为，分别画出AM、DSB、SSB（下边带）信号的频谱。',
+      'id': 1,
+      'image': 'fanyi'
+    }, {
+      'title': '大题',
+      'content': '设发送数字信息为110010101100，试分别画出OOK、2FSK、2PSK及2DPSK信号的波形示意图。(对2FSK信号,“0”对应Ts=2Tc，“1”对应Ts=Tc；其余信号Ts=Tc，其中Ts为码元周期，Tc为载波周期；对2DPSK信号，代表“0”、代表“1”，参考相位为0；对2PSK信号，代表“0”、代表“1”。)',
+      'id': 1,
+      'image': 'fanyi'
+    }
   ]
+
 })
